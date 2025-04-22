2938ea86756530dc58c29cd80286089af68ccb0f
afkpavi <rupavi2012@gmail.com>
2025-04-22 09:29:29 +0530

    shadcn init

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+import * as React from 'react';
+
+
+
+const HomePage: React.FunctionComponent<IHomePageProps> = (props) => {
+  return (
+    <Button>ProShop</Button>
+  );
+};
+
+export default HomePage;
